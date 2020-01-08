989369ce3c0d562064646c1adb491bb6640b9b9e
Tim Holmes <tim@gravitaz.co.uk>
2020-01-08 13:25:05 +0000

    add CI scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t holmestm/multi-client:latest -t holmestm/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t holmestm/multi-server:latest -t holmestm/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t holmestm/multi-worker:latest -t holmestm/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push holmestm/multi-client:latest
+docker push holmestm/multi-server:latest
+docker push holmestm/multi-worker:latest
+docker push holmestm/multi-client:$SHA
+docker push holmestm/multi-server:$SHA
+docker push holmestm/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/client-deployments server=holmestm/multi-client:$SHA
+kubectl set image deployments/server-deployments server=holmestm/multi-server:$SHA
+kubectl set image deployments/worker-deployments server=holmestm/multi-worker:$SHA
